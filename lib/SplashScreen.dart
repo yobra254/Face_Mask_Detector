8a60e00e89dac22e6dbc348722dcb412e1182c79
yobra254 <38029659+yobra254@users.noreply.github.com>
2021-03-18 11:28:25 +0300

    Addong a Splash Screen to the app

---
+++ lib/SplashScreen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class MySplashPage extends StatefulWidget {
+  @override
+  _MySplashPageState createState() => _MySplashPageState();
+}
+
+class _MySplashPageState extends State<MySplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 15,
+      title: Text(
+        'Are You Wearing A Face Mask? Lets Find Out!!!!',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+          color: Colors.blueAccent,
+        ),
+      ),
+      image: Image.asset('assets/images.png'),
+      photoSize: 130,
+      backgroundColor: Colors.white,
+      loaderColor: Colors.black,
+      loadingText: Text(
+        'A Yobra254 Product',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 16.0,
+        ),
+      ),
+    );
+  }
+}
